01e80e7687e697823e33f50a7a5fe565ab4944b7
LilMGenius <smsmeee@naver.com>
2024-12-20 03:01:39 +0900

    feat: get/patch UserPoint, PointHistory w/o PointService

--- src/point/point.controller.ts
+++ src/point/point.controller.ts
@@ -13,47 +13,74 @@ export class PointController {
         private readonly historyDb: PointHistoryTable,
     ) {}
 
-    /**
-     * TODO - 특정 유저의 포인트를 조회하는 기능을 작성해주세요.
-     */
     @Get(':id')
     async point(@Param('id') id): Promise<UserPoint> {
         const userId = Number.parseInt(id)
-        return { id: userId, point: 0, updateMillis: Date.now() }
+        const userPoint = await this.userDb.selectById(userId)
+        return userPoint
+        // return { id: userId, point: 0, updateMillis: Date.now() }
     }
 
-    /**
-     * TODO - 특정 유저의 포인트 충전/이용 내역을 조회하는 기능을 작성해주세요.
-     */
     @Get(':id/histories')
     async history(@Param('id') id): Promise<PointHistory[]> {
         const userId = Number.parseInt(id)
-        return []
+        const histories = await this.historyDb.selectAllByUserId(userId)
+        return histories
+        // return []
     }
 
-    /**
-     * TODO - 특정 유저의 포인트를 충전하는 기능을 작성해주세요.
-     */
     @Patch(':id/charge')
     async charge(
         @Param('id') id,
         @Body(ValidationPipe) pointDto: PointDto,
     ): Promise<UserPoint> {
+        // 데이터 조회
         const userId = Number.parseInt(id)
-        const amount = pointDto.amount
-        return { id: userId, point: amount, updateMillis: Date.now() }
+        const userPoint = await this.userDb.selectById(userId)
+        const newAmount = userPoint.point + pointDto.amount
+
+        // 데이터 반영
+        const updatedData = await this.userDb.insertOrUpdate(userId, newAmount)
+
+        // [+히스토리] 유저ID, 포인트량, 작업종류, 작업시간
+        await this.historyDb.insert(
+            userId,
+            pointDto.amount,
+            TransactionType.CHARGE,
+            updatedData.updateMillis
+        )
+
+        return updatedData
+        // return { id: userId, point: pointDto.amount, updateMillis: Date.now() }
     }
 
-    /**
-     * TODO - 특정 유저의 포인트를 사용하는 기능을 작성해주세요.
-     */
     @Patch(':id/use')
     async use(
         @Param('id') id,
         @Body(ValidationPipe) pointDto: PointDto,
     ): Promise<UserPoint> {
+        // 데이터 조회
         const userId = Number.parseInt(id)
-        const amount = pointDto.amount
-        return { id: userId, point: amount, updateMillis: Date.now() }
+        const userPoint = await this.userDb.selectById(userId)
+        const newAmount = userPoint.point - pointDto.amount
+
+        // [예외1] 잔고 부족
+        if (newAmount < 0) {
+            throw new Error('포인트의 잔고가 부족합니다.')
+        }
+
+        // 데이터 반영
+        const updatedData = await this.userDb.insertOrUpdate(userId, newAmount)
+
+        // [+히스토리] 유저ID, 포인트량, 작업종류, 작업시간
+        await this.historyDb.insert(
+            userId,
+            pointDto.amount,
+            TransactionType.USE,
+            updatedData.updateMillis
+        )
+
+        return updatedData
+        // return { id: userId, point: pointDto.amount, updateMillis: Date.now() }
     }
 }
